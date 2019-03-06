3323271231fc433a80042088276fbbe663728ce9
Erik Djamgarian <djamgarian0702@gmail.com>
2019-03-06 20:01:22 +0100

    added changes

--- ausschreiben.py
+++ ausschreiben.py
@@ -2,8 +2,30 @@ import requests
 
 from bs4 import BeautifulSoup
 
-data = requests.get('https://www.auftrag.at/Search.aspx?searchfilter=software&nutscode=AT')
+keywords = ["KI", "AI", "Big Data", "Data", "data", "big data", "Analytics", "analytics", "digitalisierung", "ML",
+            "Machine Learning"]
+
+headers = ""
+
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
+
+data = requests.get('https://www.auftrag.at//tenders.aspx')
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-print(data.text)
+jobs = soup.find_all('div', {'class': 'main-cont'})
+
+print(jobs.text)
+
+
+"""
+For links use 
+
+links = []
+
+for link in jobs.find_all('a'):
+    links.append(link.get('href'))
+
+"""
+
+
